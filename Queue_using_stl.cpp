17de8140b6248bbcc842efedb77262b99430f13c
fayez <50353454+fayezmohammad096@users.noreply.github.com>
2021-03-18 02:12:11 +0600

    Add files via upload

---
+++ Queue_using_stl.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<queue>
+using namespace std;
+int main(){
+    queue <int> q;
+    q.push(1);
+    q.push(10);
+    q.push(3);
+    q.push(4);
+    //q.pop();
+    while(!q.empty()){
+        int x =q.front();
+        cout<<x<<" ";
+        q.pop();
+    }
+    // int x = q.front();
+    // cout<<x<<endl;
+
+    return 0;
+}
+// queue int Name_of_uqeue
